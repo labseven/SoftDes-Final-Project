670241567d4f519b9ba316a7bfa75b986aeb800e
Adam Novotny <adam.novotny@postrock.wall>
2017-03-27 13:36:32 -0400

    Remove lidar offset

--- car.py
+++ car.py
@@ -1,12 +1,11 @@
-class Car:
+class Car():
     """
     The car. Has sensors, position, can accelerate, and turn the wheels.
     """
 
-    def __init__(self, world, init_pos, init_angle, lidar_pos, mass = 500, moment = 200):
+    def __init__(self, world, init_pos, init_angle, mass = 500, moment = 200):
         self.position = init_pos
         self.angle = init_angle
-        self.lidar_car_pos = lidar_pos
         self.velocity = [0,0]
         self.mass = mass
         self.moment = moment
@@ -17,5 +16,6 @@ class Car:
         Updates its position.
         """
 
-        self.lidar_pos[0] = self.car.position[0] + cos(angle) * self.car.lidar_pos[0] + sin(angle) * self.car.lidar_pos[1]
-        self.lidar_pos[1] = self.car.position[1] + sin(angle) * self.car.lidar_pos[0] + cos(angle) * self.car.lidar_pos[1]
+        # # Update lidar position
+        # self.lidar_pos[0] = self.car.position[0] + cos(angle) * self.car.lidar_pos[0] + sin(angle) * self.car.lidar_pos[1]
+        # self.lidar_pos[1] = self.car.position[1] + sin(angle) * self.car.lidar_pos[0] + cos(angle) * self.car.lidar_pos[1]
