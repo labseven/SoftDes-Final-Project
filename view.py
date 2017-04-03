508cb39e3d772d35c2b79d6918235e82fbf1c81e
GGRice <gretchrice4@gmail.com>
2017-04-03 15:02:53 -0400

    More merge?

--- view.py
+++ view.py
@@ -17,6 +17,8 @@ class View():
         self.objects = [("corn.png", randint(0, 900), randint(0, 900)) for x in range(100)]
         self.objects.extend([("barn.png", randint(0, 900), randint(0, 900))])
         self.world = map_in
+        self.draw_on = False
+        self.last_pos = (0, 0)
 
     def draw_scene(self, world):
         """
@@ -24,6 +26,23 @@ class View():
         """
         self.screen.fill(self.bg_color)  # Draw background color
         self.render_road(world.road)
+
+        radius = 100
+        color = (255, 255, 255)
+        e = pygame.event.wait()
+        if e.type == pygame.QUIT:
+            raise StopIteration
+        if e.type == pygame.MOUSEBUTTONDOWN:
+            world.road[e.pos[0]:e.pos[0]+radius, e.pos[1]:e.pos[1]+radius] = 255
+            self.draw_on = True
+        if e.type == pygame.MOUSEBUTTONUP:
+            self.draw_on = False
+        if e.type == pygame.MOUSEMOTION:
+            if self.draw_on:
+                world.road[e.pos[0]:e.pos[0]+10, e.pos[1]:e.pos[1]+10] = 255
+                self.roundline(world, color, e, self.last_pos,  radius)
+            self.last_pos = e.pos
+
         self.draw_decorations(self.objects)
         self.draw_car(world.car)
         pygame.display.flip()
@@ -50,7 +69,6 @@ class View():
                     (x+sin(theta)*l-cos(theta)*w, y-cos(theta)*l-sin(theta)*w)]
         pygame.draw.polygon(self.screen, car.color, vertices)  # Draw car
         pygame.draw.polygon(self.screen, (0, 0, 0), vertices, 2)  # Draw outline
-
         self.draw_lidar(car)
 
     def draw_lidar(self, car):
@@ -68,7 +86,15 @@ class View():
             self.screen.blit(img_sprite, (obj[1], obj[2]))
 
 
-
+    def roundline(self, world, color, e, end, radius):
+        start = e.pos
+        dx = end[0]-start[0]
+        dy = end[1]-start[1]
+        distance = max(abs(dx), abs(dy))
+        for i in range(distance):
+            x = int( start[0]+float(i)/distance*dx)
+            y = int( start[1]+float(i)/distance*dy)
+            world.road[x:x+radius, y:y+radius] = 255
 
 if __name__ == "__main__":
     view = View()
