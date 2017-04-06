265fa008a06d9165f5515c1232b26d0341cfdfd3
Adam Novotny <adam.novotny@postrock.wall>
2017-04-06 13:59:21 -0400

    Comment and clean up

--- sensors.py
+++ sensors.py
@@ -14,28 +14,23 @@ class Sensors():
     Lidar with n points
     """
     def __init__(self, car, road, world_size, lidar_num = 20, lidar_max_angle = pi-.01):
-        """
-        Initializes the sensor class.
-        """
-
         self.car = car
         self.road = road
         self.world_size = world_size
 
+        # Create equally spaced sensors
         self.lidar_angles = []
-        # Add equally spaced sensors
         lidar_spacing = (2 * lidar_max_angle) / (lidar_num)
 
         for i in range(lidar_num):
             self.lidar_angles.append(-lidar_max_angle + (i * lidar_spacing))
 
-    def get_lidar_data(self):
-        """
-        Outputs a list of distances.
+    def get_lidar_data(self, lidar_angles=self.lidar_angles):
+        """ Outputs a list of lidar distances.
         """
         distances = []
         hit_pos = []
-        for angle in self.lidar_angles:
+        for angle in lidar_angles:
             lidar = self.get_lidar_distance(angle)
             distances.append(lidar[0])
             hit_pos.append(lidar[1])
@@ -44,38 +39,47 @@ class Sensors():
 
 
     def get_lidar_distance(self, angle):
-        """
-        Returns the distancs from lidar position to nearest wall, in the
-        direction of angle.
+        """ Return the distance from car to the nearest wall, in the direction
+        of angle. Also returns coordinates of lidar hit.
+
+        Uses raycasting to find the hit location.
+        See (lodev.org/cgtutor/raycasting.html) for more information.
+        Return: Distance, [mapX, mapY]
         """
 
         locationX = self.car.position[0]
         locationY = self.car.position[1]
+
         # Position in map
         mapX = int(locationX)
         mapY = int(locationY)
 
-        # What the value of the map where the car is at
+        # Save the value of the map where the car currently is
+        # Ray goes until the map changes values
         try:
             curr_map_value = self.road[mapX][mapY]
         except:
+            # If off the screen
             curr_map_value = 0
-        # print(self.car.angle[0])
+
         ray_angle = angle - self.car.angle[0]
 
         dirX = sin(ray_angle)
         dirY = cos(ray_angle)
 
+        # Set deltaDist based on angle. Ifs are to remove divide by zero.
+        # sideDist is incremented by deltaDist every x or y step
         if dirX == 0:
-            deltaDistX = 0 # Might be wrong
+            deltaDistX = 0
         else:
             deltaDistX = sqrt(1 + dirY**2 / dirX**2)
 
         if dirY == 0:
-            deltaDistY = 0 # Might be wrong
+            deltaDistY = 0
         else:
             deltaDistY = sqrt(1 + dirX**2 / dirY**2)
 
+        # Set init sideDist and what direction to step
         if (dirX < 0):
             stepX = -1
             sideDistX = (locationX - mapX) * deltaDistX
@@ -90,12 +94,10 @@ class Sensors():
             stepY = 1
             sideDistY = (mapY - locationY + 1) * deltaDistY
 
-        # print("Step:", stepX,stepY, "sideDist", sideDistX, sideDistY)
-
-
         # Step through boxes until you hit a wall
         while(True):
-            # Step in the shorter side dist
+            # Step in the direction of the shorter sideDist
+            # This makes the steps follow the actual slope of the ray
             if sideDistX < sideDistY:
                 sideDistX += deltaDistX
                 mapX += stepX
@@ -104,20 +106,16 @@ class Sensors():
                 sideDistY += deltaDistY
                 mapY += stepY
                 side = 1
+
             # Hit edge of map
             if mapX <= 0 or mapX >= self.world_size[0]-1 or mapY <= 0 or mapY >= self.world_size[1]-1:
                 break
-            # if self.car.world.world_map[mapX][mapY] == 1:
+            # Hit something that is not the current map value
             if self.road[mapX][mapY] != curr_map_value:
                 break
 
-        # print("Hit at:", mapX, mapY)
-        # print("sideDist:", sideDistX, sideDistY)
-
+        # TODO: Optimize this.
+        # Can remove sqrt and then compare square of the distance
         distance = sqrt((mapX-locationX)**2 + (mapY-locationY)**2)
 
         return distance, [mapX, mapY]
-
-
-if __name__ == '__main__':
-    pass
