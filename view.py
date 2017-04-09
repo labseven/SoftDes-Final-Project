70f2515c9881895601e09d2da2611b9b62472095
Matt Brucker <mpbrucker@gmail.com>
2017-04-08 22:54:33 -0400

    Made drawing use circles instead of squares.

--- view.py
+++ view.py
@@ -24,19 +24,19 @@ class View():
 
     def build_obj_canvas(self):
         objects = [("assets/corn.png", randint(0, 999), randint(0, 999)) for x in range(5000)]
-        dirt = [("assets/dirt.png", randint(0, 999), randint(0, 999)) for x in range(500)]
+        # dirt = [("assets/dirt.png", randint(0, 999), randint(0, 999)) for x in range(500)]
         barn = [("assets/barn.png", randint(0, 900), randint(0, 900))]
         obj_surfaces = pygame.Surface(self.screen.get_size(), pygame.SRCALPHA, 32).convert_alpha()
         valid_objects = [obj for obj in objects if self.world.road[obj[1], obj[2]] == 0]
-        valid_dirt = [obj for obj in dirt if self.world.road[obj[1], obj[2]] != 0]
+        # valid_dirt = [obj for obj in dirt if self.world.road[obj[1], obj[2]] != 0]
         obj_surfaces = self.draw_decorations(valid_objects, obj_surfaces)
         obj_surfaces = self.draw_decorations(barn, obj_surfaces)
-        obj_surfaces = self.draw_decorations(valid_dirt, obj_surfaces)
+        # obj_surfaces = self.draw_decorations(valid_dirt, obj_surfaces)
 
         return obj_surfaces
 
 
-    def draw_scene(self, world):
+    def draw_scene(self, world, events):
         """
         Draws one frame of a scene.
         """
@@ -44,23 +44,24 @@ class View():
 
         radius = 100
         color = (255, 255, 255)
-        events = pygame.event.get()
         for e in events:
             if e.type == pygame.QUIT:
                 raise StopIteration
             if e.type == pygame.MOUSEBUTTONDOWN:
-                world.road[e.pos[0]:e.pos[0]+radius, e.pos[1]:e.pos[1]+radius] = 255
+                self.roundline(world, color, e, e.pos,  radius)
                 self.draw_on = True
             if e.type == pygame.MOUSEBUTTONUP:
+                # self.road_mask = self.get_road_surface(self.world.road)
                 self.draw_on = False
             if e.type == pygame.MOUSEMOTION:
                 if self.draw_on:
-                    world.road[e.pos[0]:e.pos[0]+10, e.pos[1]:e.pos[1]+10] = 255
+                    # world.road[e.pos[0]:e.pos[0]+10, e.pos[1]:e.pos[1]+10] = 255
+                    # pygame.draw.rect(self.road_mask, (150, 115, 33), (e.pos[0], e.pos[1], radius, radius), 0)
                     self.roundline(world, color, e, self.last_pos,  radius)
                 self.last_pos = e.pos
 
         self.screen.blit(self.road_mask, (0, 0))  # Mask road and background together
-        self.screen.blit(self.objs, (0, 0))
+        # self.screen.blit(self.objs, (0, 0))
         self.draw_car(self.world.car)
 
         pygame.display.flip()
@@ -70,6 +71,7 @@ class View():
         Renders the pixels for a road on the frame.
         """
         mask = pygame.Surface((road.shape[0], road.shape[1]), pygame.SRCALPHA, 32).convert_alpha()
+
         for x in range(0, road.shape[0]):
             for y in range(0, road.shape[1]):
                 if road[x,y] > 0:
@@ -111,14 +113,25 @@ class View():
         return screen
 
     def roundline(self, world, color, e, end, radius):
+        circ_surface = pygame.Surface((100, 100))
+        circ_surface.fill((0, 0, 0))
+        pygame.draw.circle(circ_surface, (255, 255, 255), (int(radius/2), int(radius/2)), int(radius/2), 0)
+
+        pix_array = pygame.surfarray.pixels_red(circ_surface)
+
+        print(pix_array)
         start = e.pos
         dx = end[0]-start[0]
         dy = end[1]-start[1]
         distance = max(abs(dx), abs(dy))
+
+        offset = int(radius/2)
         for i in range(distance):
             x = int(start[0]+float(i)/distance*dx)
             y = int(start[1]+float(i)/distance*dy)
-            world.road[x:x+radius, y:y+radius] = 255
+
+            world.road[x-offset:x+offset, y-offset:y+offset] += pix_array
+            pygame.draw.circle(self.road_mask, (150, 115, 33), (x, y), int(radius/2), 0)
 
 
 if __name__ == "__main__":
