c3f1184bc83bd181abe52d5f47621daa8832f9bc
AChapNoander <alexnoander14@gmail.com>
2017-04-17 17:51:28 -0400

    Begining Evolutionary Implementation

---
+++ evolution.py
@@ -0,0 +1,224 @@
+"""
+Evolutionary algorithm, attempts to evolve a given message string.
+
+Uses the DEAP (Distributed Evolutionary Algorithms in Python) framework,
+http://deap.readthedocs.org
+
+Usage:
+    python evolve_text.py [goal_message]
+
+Full instructions are at:
+https://sites.google.com/site/sd15spring/home/project-toolbox/evolutionary-algorithms
+"""
+
+import random
+
+import numpy    # Used for statistics
+from deap import algorithms
+from deap import base
+from deap import tools
+
+# -----------------------------------------------------------------------------
+#  Global variables
+# -----------------------------------------------------------------------------
+
+VALID_COEFF = numpy.arange(-1, 1.1, 0.1)
+print(VALID_COEFF)
+# Control whether all Autopilots are printed as they are evaluated
+VERBOSE = True
+
+
+# -----------------------------------------------------------------------------
+# Autopilot object to use in evolutionary algorithm
+# -----------------------------------------------------------------------------
+
+class FitnessMinimizeSingle(base.Fitness):
+    """
+    Class representing the fitness of a given individual, with a single
+    objective that we want to minimize (weight = -1)
+    """
+    weights = (-1.0, )
+
+
+class Autopilot(list):
+    """
+    Representation of an individual Autopilot within the population to be evolved
+
+    We represent the Autopilot as a list of coefficients (mutable) so it can
+    be more easily manipulated by the genetic operators.
+    """
+    def __init__(self, starting_string=None, min_length=4, max_length=30):
+        """
+        Create a new Autopilot individual.
+
+        If starting_string is given, initialize the Autopilot with the
+        provided string message. Otherwise, initialize to a random string
+        message with length between min_length and max_length.
+        """
+        # Want to minimize a single objective: distance from the goal message
+        self.fitness = FitnessMinimizeSingle()
+
+        # Populate Autopilot using starting_string, if given
+        if starting_string:
+            self.extend(list(starting_string))
+
+        # Otherwise, select an initial length between min and max
+        # and populate Autopilot with that many random characters
+        else:
+            initial_length = random.randint(min_length, max_length)
+            for i in range(initial_length):
+                # TODO: random choice of coefficients
+                pass
+
+    def __repr__(self):
+        """Return a string representation of the Autopilot"""
+        # Note: __repr__ (if it exists) is called by __str__. It should provide
+        #       the most unambiguous representation of the object possible, and
+        #       ideally eval(repr(obj)) == obj
+        # See also: http://stackoverflow.com/questions/1436703
+        template = '{cls}({val!r})'
+        return template.format(cls=self.__class__.__name__,     # "Autopilot"
+                               val=self.get_text())
+
+    def get_text(self):
+        """Return Autopilot as string (rather than actual list of chars)"""
+        return "".join(self)
+
+
+# -----------------------------------------------------------------------------
+# Genetic operators
+# -----------------------------------------------------------------------------
+
+
+def evaluate_driving(message, goal_text, verbose=VERBOSE):
+    """
+    Given a Autopilot and a goal_text string, return the Levenshtein distance
+    between the Autopilot and the goal_text as a length 1 tuple.
+    If verbose is True, print each Autopilot as it is evaluated.
+    """
+    # TODO: Implement method which determines fitness of a given individual
+
+    if verbose:
+        print("{msg!s}\t[Distance: {dst!s}]".format(msg=message, dst=distance))
+    return (distance, )     # Length 1 tuple, required by DEAP
+
+
+def mutate_autopilots(coefficients, prob_ins=0.05, prob_del=0.05, prob_sub=0.05):
+    """
+    Given a Autopilot and independent probabilities for each mutation type,
+    return a length 1 tuple containing the mutated Autopilot.
+
+    Possible mutations are:
+        Insertion:      Insert a random (legal) coefficient somewhere into
+                        the Autopilot
+        Deletion:       Delete one of the coefficients from the Autopilot
+        Substitution:   Replace one coefficients of the Autopilot with a random
+                        (legal) coefficient
+
+    >>> mutate_autopilots('hello', 1, 1, 1)
+    """
+    coefficients = list(coefficients)
+
+    if random.random() < prob_ins:
+        index = random.randint(0, len(coefficients) - 1)
+        coefficients.insert(index, random.choice(VALID_COEFF))
+
+    if random.random() < prob_del:
+        index = random.randint(0, len(coefficients) - 1)
+        coefficients.pop(index)
+
+    if random.random() < prob_sub:
+        index = random.randint(0, len(coefficients) - 1)
+        coefficients[index] = random.choice(VALID_COEFF)
+
+    message = ''.join(coefficients)
+
+    return (message, )   # Length 1 tuple, required by DEAP
+
+
+# -----------------------------------------------------------------------------
+# DEAP Toolbox and Algorithm setup
+# -----------------------------------------------------------------------------
+
+def get_toolbox(text):
+    """Return DEAP Toolbox configured to evolve given 'text' string"""
+
+    # The DEAP Toolbox allows you to register aliases for functions,
+    # which can then be called as "toolbox.function"
+    toolbox = base.Toolbox()
+
+    # Creating population to be evolved
+    toolbox.register("individual", Autopilot)
+    toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+
+    # Genetic operators
+    toolbox.register("evaluate", evaluate_driving, goal_text=text)
+    toolbox.register("mate", tools.cxTwoPoint)
+    toolbox.register("mutate", mutate_autopilots)
+    toolbox.register("select", tools.selTournament, tournsize=3)
+
+    # NOTE: You can also pass function arguments as you define aliases, e.g.
+    #   toolbox.register("individual", Autopilot, max_length=200)
+    #   toolbox.register("mutate", mutate_autopilots, prob_sub=0.18)
+
+    return toolbox
+
+
+def evolve_autopilot(text):
+    """Use evolutionary algorithm (EA) to evolve 'text' string"""
+
+    # Set random number generator initial seed so that results are repeatable.
+    # See: https://docs.python.org/2/library/random.html#random.seed
+    #      and http://xkcd.com/221
+    random.seed(4)
+
+    # Get configured toolbox and create a population of random Autopilots
+    toolbox = get_toolbox(text)
+    pop = toolbox.population(n=300)
+
+    # Collect statistics as the EA runs
+    stats = tools.Statistics(lambda ind: ind.fitness.values)
+    stats.register("avg", numpy.mean)
+    stats.register("std", numpy.std)
+    stats.register("min", numpy.min)
+    stats.register("max", numpy.max)
+
+    # Run simple EA
+    # (See: http://deap.gel.ulaval.ca/doc/dev/api/algo.html for details)
+    pop, log = algorithms.eaSimple(pop,
+                                   toolbox,
+                                   cxpb=0.5,    # Prob. of crossover (mating)
+                                   mutpb=0.2,   # Probability of mutation
+                                   ngen=500,    # Num. of generations to run
+                                   stats=stats)
+
+    return pop, log
+
+
+# -----------------------------------------------------------------------------
+# Run if called from the command line
+# -----------------------------------------------------------------------------
+if __name__ == "__main__":
+
+    """    import doctest
+    doctest.testmod()"""
+
+    # Get goal message from command line (optional)
+    import sys
+    if len(sys.argv) == 1:
+        # Default goal of the evolutionary algorithm if not specified.
+        # Pretty much the opposite of http://xkcd.com/534
+        goal = "SKYNET IS NOW ONLINE"
+    else:
+        goal = " ".join(sys.argv[1:])
+
+    # Verify that specified goal contains only known valid characters
+    # (otherwise we'll never be able to evolve that string)
+    for char in goal:
+        if char not in VALID_COEFF:
+            msg = "Given text {goal!r} contains illegal character {char!r}.\n"
+            msg += "Valid set: {val!r}\n"
+            raise ValueError(msg.format(goal=goal, char=char, val=VALID_COEFF))
+
+    # Run evolutionary algorithm
+    pop, log = evolve_autopilot(goal)
